ffc776a5e948581d65aa45755fb6947f69960e9a
kinipk <cloverfieldor@gmail.com>
2023-11-17 11:28:22 -0300

    Language changes and some cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,7 +1,8 @@
 const current_items = JSON.parse(localStorage.getItem("inCart"))
 const product_list = document.getElementById("cart_list")
+var complete_price = 0
 for (item of current_items) {
-   var product_base = ` <li class="Cart_Product" id="${item.name}">
+	var product_base = ` <li class="Cart_Product" id="${item.name}">
 				<table>
 					<tr class="Table">
 						<td rowspan="2"class="Product_Img">
@@ -24,8 +25,9 @@ for (item of current_items) {
 				</table>
 			</li>
             `
-    console.log(typeof(product_base))
-    product_list.insertAdjacentHTML("afterbegin", product_base)
+	complete_price = item.price + complete_price
+	console.log(typeof (product_base))
+	product_list.insertAdjacentHTML("afterbegin", product_base)
 
 }
 
@@ -36,8 +38,8 @@ function changeTotal(id) {
 	const price = current_element.getElementsByClassName("IndividualP")[0]
 	const real_price = parseFloat(price.innerHTML.slice(2))
 	const real_quantity = parseInt(quantity.value)
-	if(real_quantity === 0) {
-		const name = current_element.getElementsByClassName("Product_Name")[0].innerHTML.replace(/(\r\n|\n|\r|\t)/gm,"")
+	const name = current_element.getElementsByClassName("Product_Name")[0].innerHTML.replace(/(\r\n|\n|\r|\t)/gm, "")
+	if (real_quantity === 0) {
 		current_element.remove()
 		for (const i in current_items) {
 			console.log(name, current_items[i].name)
@@ -45,6 +47,13 @@ function changeTotal(id) {
 				current_items.splice(i, 1)
 				localStorage.setItem("inCart", JSON.stringify(current_items))
 				return
+			}
+		}
+	} else {
+		for (const i in current_items) {
+			if (name == current_items[i].name) {
+				current_items[i].quant = real_quantity
+				localStorage.setItem("inCart", JSON.stringify(current_items))
 			}
 		}
 	}
